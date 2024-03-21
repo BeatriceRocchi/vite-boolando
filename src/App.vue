437e6c1ac0d7fb02bbaf770d2c6331cd05076b4f
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-03-21 14:22:06 +0100

    [add] vue components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script>
-export default {};
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Footer,
+    Main,
+  },
+};
 </script>
 
 <template>
-  <div>Vite Boolando</div>
+  <Header />
+  <Main />
+  <Footer />
 </template>
 
 <style lang="scss"></style>
